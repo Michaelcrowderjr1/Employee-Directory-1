08a07edcd4ec09a1048e9f11561719526d15535f
drclever <drc0625@aol.com>
2021-02-15 20:42:23 -0500

    Cleanup and standardize code to match other code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import EmployeesContainer from "./components/Container/EmployeesContainer";
+import Header from "./components/Header/index.js";
+import EmployeesContainer from "./components/Container/EmployeesContainer.js";
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <EmployeesContainer />
-    </>
-  );
+function App() {
+    return (
+        <>
+            <Header />
+            <EmployeesContainer />
+        </>
+    );
 };
 
 export default App;
